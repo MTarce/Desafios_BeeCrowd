a6b6d0d03d3ff7ca323ae9187da929eac6bc7f64
MTarce <matheus.t.arce@gmail.com>
2025-04-05 20:40:31 -0400

    Arrumar espaco

--- Beginner/AgeInDays_1020.c
+++ Beginner/AgeInDays_1020.c
@@ -16,8 +16,8 @@ int main()
     mes = restoAno/30;
     restoMes = restoAno%30;
 
-    printf("%d ano (s)\n", ano);
-    printf("%d mes (es)\n", mes);
-    printf("%d dia (s)\n",restoMes);
+    printf("%d ano(s)\n",ano);
+    printf("%d mes(es)\n",mes);
+    printf("%d dia(s)\n",restoMes);
     return 0;
 }
