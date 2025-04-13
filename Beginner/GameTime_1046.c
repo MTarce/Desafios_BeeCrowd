b7668523acb5d737675e58087b6f1e2f35f2aa17
MTarce <matheus.t.arce@gmail.com>
2025-04-12 20:50:32 -0400

    Resolvendo desafio 1046 do beecrowd

---
+++ Beginner/GameTime_1046.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int n1,n2,duration;
+
+int main() 
+{
+    scanf("%d %d",&n1,&n2);
+    if(n1 < n2)
+    {
+        int mod = abs(n1-n2);
+        printf("O JOGO DUROU %d HORA(S)\n",mod); 
+    }
+    else{
+        int mod = abs(n1-n2);
+        int duration = 24 - mod;
+        printf("O JOGO DUROU %d HORA(S)\n",duration); 
+    }
+    return 0;
+}
