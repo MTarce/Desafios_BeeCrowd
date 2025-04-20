dbb2dbd46bd618a12024562389db4ca8451195b2
MTarce <matheus.t.arce@gmail.com>
2025-04-19 22:42:45 -0400

    Resolvendo desafio 1075

---
+++ Beginner/1075_Remaining2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() 
+{
+    int n;
+    scanf("%d",&n);
+
+    for (size_t i = 1; i < 10000; i++)
+    {
+        if(i%n == 2)
+        {
+            printf("%d\n",i);
+        }
+        
+    }
+    
+
+ 
+    return 0;
+}
