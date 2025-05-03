cb7364d92cbe23610f6f4f60de11c507c582dfa2
MTarce <matheus.t.arce@gmail.com>
2025-05-02 23:28:35 -0400

    Resolvendo desafio 1113

---
+++ Beginner/1113_AscendingAndDescending.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main() 
+{
+    int x,y;
+    do
+    {
+        scanf("%d %d", &x,&y);
+        if(x==y)
+        {
+            break;
+        }
+        else if(x > y)
+        {
+            printf("Decrescente\n");
+        }
+        else{
+            printf("Crescente\n");
+        }
+
+    } while (1);
+    
+ 
+ 
+    return 0;
+}
