dc02894c459fb163b323907416cbd67219f246a5
Matheus Tenazor arce <matheus.t.arce@gmail.com>
2025-05-05 11:51:17 -0400

    Resolvendo desafio 1117

---
+++ Beginner/1117_ScoreValidation.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    float score,sum=0;
+    int count =0;
+    for (;;)
+    {
+        scanf("%f", &score);
+        if (score >= 0 && score <= 10)
+        {
+            sum = sum +score;
+            count++;
+
+            if (count == 2)
+            {
+                printf("media = %.2f\n", sum / 2);
+                break;
+            }
+        }
+        else
+        {
+            printf("nota invalida\n");
+        }
+    }
+
+    return 0;
+}
