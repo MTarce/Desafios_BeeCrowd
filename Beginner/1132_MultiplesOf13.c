d7597bdcbcb480987e7a22dde7212cdd7e4baf2e
Matheus Tenazor <matheus.t.arce@gmail.com>
2025-05-26 16:43:27 -0400

    Resolvendo desafio 1132

---
+++ Beginner/1132_MultiplesOf13.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, sum = 0;
+    scanf("%d %d", &x, &y);
+    if (x < y)
+    {
+
+        for (int i = x; i <= y; i++)
+        {
+            if (i % 13 != 0)
+            {
+                sum = sum + i;
+            }
+        }
+        printf("%d", sum);
+    }
+    else if(x>y)
+    {
+        for (int i = x; i >= y; i--)
+        {
+            if (i % 13 != 0)
+            {
+                sum = sum + i;
+            }
+        }
+        printf("%d\n", sum);
+        
+    }
+
+    return 0;
+}
