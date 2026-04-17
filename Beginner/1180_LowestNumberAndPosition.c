475b93dd9c86d317d139181fe7ae61351566c69d
MTarce <matheus.institucional@gmail.com>
2026-04-17 19:15:11 -0400

    Atualizando usando pointer

--- Beginner/1180_LowestNumberAndPosition.c
+++ Beginner/1180_LowestNumberAndPosition.c
@@ -29,3 +29,43 @@ int main(){
 
     return 0;
 }
+
+/*----------------------------------------------------
+#include <stdio.h>
+ 
+int main() {
+
+    int N;
+    scanf("%d",&N);
+
+    int X[N];
+
+    int *p = NULL;
+    p = X;
+    // pointer prt aponta para o endereço de p == &X[0]
+    // p é o ponteiro para o array.
+    //ptr++ indica que estou caminhando na memoria.
+    for (int *ptr = p; ptr < p+N; ptr++)
+    {
+        scanf("%d",ptr);
+    }
+
+    int *ptr = p; // ponteira que vai varrer meu array.
+    int menor = *p; // menor recebe meu primeiro elemento de p
+    int *pos = p;
+
+    while (ptr < p+N)
+    {
+        if (*ptr < menor)
+        {
+            menor = *ptr;
+            pos = ptr;
+        }
+        ptr++;
+    }
+    
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %ld\n", pos - p); // diferença de ponteiros
+    return 0;
+}
+*/
